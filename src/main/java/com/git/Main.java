bb911b06588e3291a8beacc6b08d21e25a2cc361
Miguel García <miguel.garciagonzalez@gruposantander.com>
2024-09-23 17:54:43 +0200

    Updating and creating enhanced version of greeting feature

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -12,11 +12,11 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
+        System.out.println("Welcome! Please, enter your name: ");
 
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + name + "! Welcome");
+        System.out.println("Hello " + name + "! This is the enhanced greeting version!");
 
         scanner.close();
     }
