2b896eac71ea6b9a0b3e310008a4a4b38b7f7c77
Miguel García <miguel.garciagonzalez@gruposantander.com>
2024-09-23 17:17:23 +0200

    Updating greeting feature

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello " + name + "! Welcome");
 
         scanner.close();
     }
